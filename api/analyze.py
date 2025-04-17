ff76bc9b3be7d778243898a74439bdb1b04359ee
Artyom30303 <t.ak.akopyan@gmail.com>
2025-04-17 14:58:48 +0500

    Create analyze.py

---
+++ api/analyze.py
@@ -0,0 +1,10 @@
+
+from fastapi import FastAPI
+from analyzer.core import analyze_symbol
+
+app = FastAPI()
+
+@app.get("/analyze")
+def analyze(symbol: str = "BTC/USDT"):
+    result = analyze_symbol(symbol)
+    return result

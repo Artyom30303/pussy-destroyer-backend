4188895e27d88d84532c9e0d566600903f8580f6
Artyom30303 <t.ak.akopyan@gmail.com>
2025-04-17 14:54:31 +0500

    Delete api directory

--- api/analyze.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from analyzer.core import analyze_symbol
-
-app = FastAPI()
-
-@app.get("/analyze")
-def analyze(symbol: str = "BTC/USDT"):
-    result = analyze_symbol(symbol=symbol)
-    return result
